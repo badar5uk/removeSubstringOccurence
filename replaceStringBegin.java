2406f6c2bed09e27d0664177e37be7a2e97727ba
bader <bader5uk@hotmail.com>
2024-09-25 11:26:52 +0400

    Adjusted code using substring method

--- replaceStringBegin.java
+++ replaceStringBegin.java
@@ -2,10 +2,14 @@ public class replaceStringBegin {
 
     public static void main(String[] arg) {
         String firstString = "HelloHe";
-        String secaondString = "He";
-        boolean doesContain = firstString.contains(secaondString);
-        if(doesContain){
-            firstString = firstString.replace("He","");
+        String newStr;
+
+        if (firstString.substring(0, 2).equals(firstString.substring(firstString.length() - 2))) {
+            newStr = firstString.substring(2);
+            System.out.println(newStr);
+        } else if (firstString.length() == 2) {
+            System.out.println();
+        } else {
             System.out.println(firstString);
         }
     }
